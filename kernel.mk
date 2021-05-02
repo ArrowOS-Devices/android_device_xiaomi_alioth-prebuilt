be346f348c83a8a192cba58d2bcee558a83aceb2
Michael P <palaych@arrowos.net>
2021-05-02 21:34:52 +0300

    alioth: add dtb, Image, dtbo and modules
    * V12.0.5.0.RKHEUXM

---
+++ kernel.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Kernel Modules
+KERNEL_MODULES_DIR := $(LOCAL_PATH)/modules
+
+KERNEL_MODULES_COPY := $(TARGET_COPY_OUT_VENDOR)/lib/modules
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(KERNEL_MODULES_DIR),$(KERNEL_MODULES_COPY))
